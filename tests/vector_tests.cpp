5a5d5eadded15a943ea95c3f96cfa6e19af4bf21
dylanc95 <dylanc95@proton.me>
2026-08-11 21:26:52 -0500

    Added Catch2

--- tests/vector_tests.cpp
+++ tests/vector_tests.cpp
@@ -1,33 +1,33 @@
 #include <ds/vector.hpp>
-#include <cassert>
+#include <catch2/catch_test_macros.hpp>
 
-int main() {
+TEST_CASE("Vectors can take data types", "[vector][push_back]") {
     
-    ds::Vector<int> num;
+    SECTION("Integers") {
+        ds::Vector<int> arr;
 
-    assert(num.empty());
+        arr.push_back(10);
+        arr.push_back(20);
+        arr.push_back(30);
 
-    num.push_back(10);
-    num.push_back(20);
-
-    assert(num.size() == 2);
-    assert(num.at(0) == 10);
-    assert(num.at(1) == 20);
-
-    num.pop_back();
-    assert(num.size() == 1);
-    assert(num.at(0) == 10);
-
-    num.clear();
-    assert(num.empty());
-
-    for (int i = 0; i < 15; i++) {
-        num.push_back(i);
+        REQUIRE(arr[0] == 10);
+        REQUIRE(arr[1] == 20);
+        REQUIRE(arr[2] == 30);
     }
 
-    assert(num.capacity() == 20);
-    assert(num.front() == 0);
-    assert(num.back() == 14);
+    SECTION("Doubles") {
+        ds::Vector<double> arr;
 
-    return 0;
+        arr.push_back(2.5);
+        arr.push_back(3.14);
+        arr.push_back(60.2);
+
+        REQUIRE(arr[0] == 2.5);
+        REQUIRE(arr[1] == 3.14);
+        REQUIRE(arr[2] == 60.2);
+    }
+
+    SECTION("Boolean") {
+        
+    }
 }
