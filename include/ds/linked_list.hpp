dde655f4480e5f53481ec7771bed67be3ea50614
dylanc95 <dylanc95@proton.me>
2026-08-15 21:05:24 -0500

    Still working on linked list

--- include/ds/linked_list.hpp
+++ include/ds/linked_list.hpp
@@ -13,12 +13,38 @@
         private:
         struct Node {
             T data_;
-            std::unique_ptr<Node> next;
+            std::unique_ptr<Node> next_;
+
+            Node(const T& value) : data_(value), next_(nullptr) {}
         };
-        std::unique_ptr<Node> head_;
+        std::unique_ptr<Node> head_ = nullptr;
         Node* tail_ = nullptr;
         std::size_t size_ = 0;
 
         public:
-    }
+        void insertAtBeginning(const T& value) {
+            auto newNode = std::make_unique<Node>(value);
+
+            newNode->next_ = std::move(head_);
+            head_ = std::move(newNode);
+
+            if (size_ == 0) {
+                tail_ = head_.get();
+            }
+
+            size_++;
+        };
+
+        void insertAtEnd(const T& value) {
+            if (size_ == 0) {
+                insertAtBeginning(value);
+            } else{
+                auto newNode = std::make_unique<Node>(value);
+
+                tail_->next_ = std::move(newNode);
+                tail_ = tail_->next_.get();
+                size_++;
+            };
+        };
+    };
  }
---
+++ tests/linked_list_tests.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "../include/ds/linked_list.hpp"
+
+int main() {
+
+    ds::LinkedList<int> list;
+
+    list.insertAtEnd(10);
+
+    return 0;
+}
